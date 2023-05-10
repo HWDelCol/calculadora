cf5bddc49399d8d4545769973724badb6c5334d5
H Del Col <h.delcol@outlook.com>
2023-05-10 18:06:23 -0300

    atualized

--- calculadora.c
+++ calculadora.c
@@ -7,7 +7,7 @@ int main() {
     printf("Digite um operador (+, -, *, /): ");
     scanf("%c", &operador);
 
-    printf("Digite dois números: ");
+    printf("Digite dois numeros: ");
     scanf("%f %f", &num1, &num2);
 
     switch (operador) {
@@ -24,9 +24,9 @@ int main() {
             resultado = num1 / num2;
             break;
         default:
-            printf("Operador unválido.");
+            printf("Operador invalido.");
             return 1;
     }
-    printf("%,2f %c %.2f = %.2f", num1, operador, num2, resultado);
+    printf("%.2f %c %.2f = %.2f", num1, operador, num2, resultado);
     return 0;
 }
