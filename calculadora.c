644ef05185616909abd3ca990eb9b4979d606a34
H Del Col <h.delcol@outlook.com>
2023-05-10 17:27:08 -0300

    calculadora simples

---
+++ calculadora.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    char operador;
+    float num1, num2, resultado;
+
+    printf("Digite um operador (+, -, *, /): ");
+    scanf("%c", &operador);
+
+    printf("Digite dois números: ");
+    scanf("%f %f", &num1, &num2);
+
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 -  num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            resultado = num1 / num2;
+            break;
+        default:
+            printf("Operador unválido.");
+            return 1;
+    }
+    printf("%,2f %c %.2f = %.2f", num1, operador, num2, resultado);
+    return 0;
+}
